17b4f26387c74c99a77f3b764f389eed684f8415
rodrigues <rodriguescla2@gmail.com>
2024-06-12 07:17:37 -0300

    inicio

---
+++ src/components/EssentialLink.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class=" justify-content-center rounded-4" >
+    <button
+      class="btn sidebar-btn"
+      @click="toggleSelected"
+    >
+      Clique em mim
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const isSelected = ref(false);
+
+const toggleSelected = () => {
+  isSelected.value = !isSelected.value;
+};
+
+defineOptions({
+  name: 'EssentialLink'
+});
+
+export interface EssentialLinkProps {
+  title: string;
+  caption?: string;
+  link?: string;
+  icon?: string;
+};
+
+withDefaults(defineProps<EssentialLinkProps>(), {
+  caption: '',
+  link: '#',
+  icon: '',
+});
+</script>
+
+<style scoped>
+.sidebar-btn {
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+}
+
+
+.sidebar-btn:hover {
+  background-color: #5d87ff;
+  color: white;
+}
+
+.sidebar-btn.selected:hover {
+  background-color:  #5d87ff;
+  opacity: 0.75;
+  color: white;
+}
+</style>
---
+++ src/components/MainSidebar.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <h6 class="text-grey-10 m-20">DP-Trials</h6>
+
+    <div class=" row justify-content-center">
+      <router-link
+        class=" p-10 sidebar-btn d-flex  "
+        to="/"
+      >
+        <i class="ms-10 me-10 flaticon-logout"></i>
+        <span class="title fw-semibold ms-10 me-10 ">Contact</span>
+      </router-link>
+
+      <router-link
+        class=" p-10 card-link-btn d-flex justify-self-center"
+        to="/clients"
+      >
+        <i class="flaticon-logout"></i>
+        <span class="title">Logout2</span>
+      </router-link>
+
+    </div>
+  </div>
+</template>
+
+
+<script setup lang="ts">
+</script>
+
+
+<style scoped lang="sass">
+@import 'src/css/quasar.variables.scss'
+.sidebar-btn
+  color: $grey-7
+  border-radius: 10px
+  width: 70%
+  text-decoration: none
+
+
+.sidebar-btn:hover
+  background-color: $blue-1
+  opacity: 75%
+  color: $blue-9
+
+.router-link-active
+  background-color: $blue-6
+  color: white
+
+.router-link-active:hover
+  background-color: $blue-6
+  color: white
+  opacity: 100%
+
+
+
+</style>
